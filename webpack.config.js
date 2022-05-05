c425c2b4c6fab6c6bf9ee8fd8aacb34799ae629e
coffeelovinmonster <redmanmeister@googlemail.com>
2022-05-05 18:45:19 +0100

    second attempt at the restructure thing

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,6 @@ module.exports = {
     entry:'./npm-app/src/index.js',
     output: {
         filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'npm-app/dist'),
     },
 };
